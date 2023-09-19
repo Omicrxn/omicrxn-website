3d9c2fafdb0c29c7eb6619037bf91e17ec14e687
Omicrxn <alexavila0128@gmail.com>
2023-09-19 08:58:12 +0200

    should animate added to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function AboutPage() {
   return (
-    <PageWrapper className="flex min-h-screen flex-col items-start justify-center bg-omi-white">
+    <PageWrapper shouldAnimate={true} className="flex min-h-screen flex-col items-start justify-center bg-omi-white">
       <div >
         AboutPage
       </div>
